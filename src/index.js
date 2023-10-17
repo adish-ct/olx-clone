750a804c18d1ee49e88d968db298b0c44cda35ce
adish-ct <adishathu525@gmail.com>
2023-10-17 18:55:23 +0530

    login logic implemented

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-import { FirebaseContext } from "./store/Context";
+import { FirebaseProvider } from "./store/Context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <FirebaseContext.Provider>
+    <FirebaseProvider>
       <Router>
         <App />
       </Router>
-    </FirebaseContext.Provider>
+    </FirebaseProvider>
   </React.StrictMode>
 );
